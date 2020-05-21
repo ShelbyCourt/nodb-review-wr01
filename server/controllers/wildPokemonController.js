f60bac1da8e72339c5fe5800c180d8b7ebb6e608
ShelbyCourt <shelby.b.courtney@gmail.com>
2020-05-21 09:56:32 -0700

    Backend Code Skeleton

---
+++ server/controllers/wildPokemonController.js
@@ -0,0 +1,5 @@
+module.exports = {
+    getThreeRandomPokemon: (req, res) => {
+
+    }
+}
---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const wildCtrl = require('./controllers/wildPokemonController')
+const caughtCtrl = require('./controllers/caughtPokemonController')
+const SERVER_PORT = 4232
+
+app.use(express.json())
+
+//Wild Pokemon endpoints
+app.get('/api/wild-pokemon', wildCtrl.getThreeRandomPokemon)
+
+//Caught Pokemon Endpoints
+app.get('/api/pokemon', caughtCtrl.getCaughtPokemon)
+app.post('api/pokemon', caughtCtrl.catchPokemon)
+app.put('/api/pokemon/:pokemon_id', caughtCtrl.editPokemonName)
+app.delete('/api/pokemon/:pokemon_id', caughtCtrl.deletePokemon)
+
+
+app.listen(SERVER_PORT, () =>
+  console.log(`Catching em all on port ${SERVER_PORT}`)
+)
